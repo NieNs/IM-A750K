ecab27c3963ab8f0204503a09cb0c6073d808f77
NieNs <and_roid_@naver.com>
2012-08-07 13:31:07 +0900

    first commit

---
+++ obj/KERNEL_OBJ/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 22 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#22 PREEMPT Tue Aug 7 12:48:23 KST 2012"
+#define LINUX_COMPILE_TIME "12:48:23"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "nien-RV418-RV518-RV718"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
